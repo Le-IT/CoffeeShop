ce7f9cb251bab3646be1c8fc4f0c475e1a5a6d36
Manoel Kraus <manoel.kraus.fgj@fh-zwickau.de>
2019-04-26 09:34:18 +0200

    Security-Update

--- src/main/java/whz/informatik/coffeeshop/shop/boundary/AdministrationController.java
+++ src/main/java/whz/informatik/coffeeshop/shop/boundary/AdministrationController.java
@@ -4,6 +4,7 @@ package whz.informatik.coffeeshop.shop.boundary;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,15 +26,11 @@ public class AdministrationController {
         this.customerService = customerService;
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @RequestMapping(value = {"/users_managed"})
     public String getAdministrationPage(Model model) {
         List<CustomerDTO> customers = customerService.getAllDTO();
         model.addAttribute("listCustomers", customers);
         return "administration";
     }
-
-
-
-
-
 }
